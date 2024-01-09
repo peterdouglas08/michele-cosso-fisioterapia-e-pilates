1465091f547e5f3b9752cd362ca224934240e31d
Peter Douglas <peterdouglas08@gmail.com>
2024-01-09 20:44:56 -0300

    atualizando e corrigindo erros do site

--- src/js/form.js
+++ src/js/form.js
@@ -1,6 +1,6 @@
-const formulario = document.getElementById("form");
 const dadosUsuario = document.querySelectorAll(".dados");
-const botaoEnviar = document.querySelector(".enviar");
+const botaoEnviar = document.getElementById("botao-enviar");
+const botaoLimpar = document.getElementById("botao-limpar");
 
 botaoEnviar.addEventListener("click", (event) => {
   event.preventDefault();
@@ -18,10 +18,10 @@ botaoEnviar.addEventListener("click", (event) => {
   });
 });
 
-formulario.addEventListener("click", () => {
-  if (formulario.value >= input.lenght) {
-    input.classList.add("aceito");
-    input.classList.remove("recusado");
-    input.nextElementSibling.classList.add("alertaOff");
-  }
-});
+botaoLimpar.addEventListener("click", () => {
+  dadosUsuario.forEach((input) => {
+    input.classList.contains("aceito") && input.classList.remove("aceito")
+    input.classList.contains("recusado") && input.classList.remove("recusado")
+    input.nextElementSibling.classList.contains("alertaOff") || input.nextElementSibling.classList.add("alertaOff")
+  })
+})
